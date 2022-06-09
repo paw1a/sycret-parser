cb9e918cdb20ee91ca126c43a69766a3891f9fbb
paw1a <paw1a@yandex.ru>
2022-06-09 10:20:09 +0300

    add http server and request/response structs

--- internal/app/app.go
+++ internal/app/app.go
@@ -2,19 +2,16 @@ package app
 
 import (
 	"fmt"
-	"github.com/beevik/etree"
+	"github.com/paw1a/sycret-parser/internal/handler"
+	"log"
+	"net/http"
+)
+
+const (
+	Port = 8080
 )
 
 func Run() {
-	doc := etree.NewDocument()
-	if err := doc.ReadFromFile("forma_025u.doc"); err != nil {
-		panic(err)
-	}
-
-	for _, elem := range doc.FindElements("//text") {
-		attr := elem.SelectAttr("field").Value
-		textElem := elem.SelectElement("r").SelectElement("t")
-		textElem.SetText(fmt.Sprintf("%s ", attr))
-	}
-	doc.WriteToFile("out.doc")
+	http.HandleFunc("/api/doc", handler.DocEndpoint)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", Port), nil))
 }
--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,1 +1,42 @@
 package handler
+
+import (
+	"fmt"
+	"github.com/beevik/etree"
+	"net/http"
+)
+
+type SycretAPIRequest struct {
+	Text     string `json:"text"`
+	RecordID string `json:"recordid"`
+}
+
+type SycretAPIResponse struct {
+	Result            int    `json:"result"`
+	ResultDescription string `json:"resultdescription"`
+	ResultData        string `json:"resultdata"`
+}
+
+type DocParserRequest struct {
+	URLTemplate string `json:"URLTemplate"`
+	RecordID    string `json:"RecordID"`
+}
+
+type DocParserResponse struct {
+	URLWord string `json:"URLWord"`
+}
+
+func DocEndpoint(w http.ResponseWriter, r *http.Request) {
+	doc := etree.NewDocument()
+
+	if err := doc.ReadFromFile("forma_025u.doc"); err != nil {
+		panic(err)
+	}
+
+	for _, elem := range doc.FindElements("//text") {
+		attr := elem.SelectAttr("field").Value
+		textElem := elem.SelectElement("r").SelectElement("t")
+		textElem.SetText(fmt.Sprintf("%s ", attr))
+	}
+	doc.WriteToFile("out.doc")
+}
