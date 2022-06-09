fae7fdce15a08823457617d1d50645c20558cacb
paw1a <paw1a@yandex.ru>
2022-06-09 20:30:50 +0300

    add response with file url

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -48,5 +48,13 @@ func DocEndpoint(w http.ResponseWriter, r *http.Request) {
 	filename := fmt.Sprintf("%s.doc",
 		time.Now().Format("2006-01-02 15-04-05"))
 
-	_, err = storage.UploadDocument(resultDoc, filename)
+	url, err := storage.UploadDocument(resultDoc, filename)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	docResponse := DocParserResponse{URLWord: url}
+	w.Header().Set("Content-Type", "application/json")
+	err = json.NewEncoder(w).Encode(docResponse)
 }
